daa634e6b4984bcf2b32429e66d8c71ca955ebf0
atibek0va <73632167+atibek0va@users.noreply.github.com>
2023-05-19 11:44:26 +0600

    test

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -7,38 +7,38 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
-//     WebDriver driver;
+    WebDriver driver;
 
-//     @FindBy(xpath = "/html/body/nav/div[1]/ul/li[5]/a")
-//     WebElement getLoginButton;
+    @FindBy(xpath = "/html/body/nav/div[1]/ul/li[5]/a")
+    WebElement getLoginButton;
 
-//     @FindBy(id = "loginusername")
-//      WebElement usernameField;
+    @FindBy(id = "loginusername")
+     WebElement usernameField;
 
-//     @FindBy(id = "loginpassword")
-//      WebElement passwordField;
+    @FindBy(id = "loginpassword")
+     WebElement passwordField;
 
-//     @FindBy(xpath = "//button[contains(text(), 'Log in')]")
-//      WebElement loginButton;
+    @FindBy(xpath = "//button[contains(text(), 'Log in')]")
+     WebElement loginButton;
 
-//     public LoginPage(WebDriver driver) {
-//         this.driver = driver;
-//         PageFactory.initElements(driver, this);
-//     }
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
-//     public void clickLogin(){
-//         getLoginButton.click();
-//     }
+    public void clickLogin(){
+        getLoginButton.click();
+    }
 
-//     public void enterUsername(String username) {
-//         usernameField.sendKeys(username);
-//     }
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
 
-//     public void enterPassword(String password) {
-//         passwordField.sendKeys(password);
-//     }
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
 
-//     public void clickLoginButton() {
-//         loginButton.click();
-//     }
+    public void clickLoginButton() {
+        loginButton.click();
+    }
 }
